d2df3570aa288169ab6437147314ba2026709fa1
Zakaria EL BAZI <za.elbazi@insea.ac.ma>
2019-04-03 11:09:31 +0200

    Update som_fraud.py

--- som_fraud.py
+++ som_fraud.py
@@ -20,6 +20,23 @@ som = MiniSom(x = 10, y = 10, input_len = 30, sigma = 1.0, learning_rate = 0.5)
 som.random_weights_init(X)
 som.train_random(data = X, num_iteration = 100)
 
+# Visualizing the results
+from pylab import bone, pcolor, colorbar, plot, show
+bone()
+pcolor(som.distance_map().T)
+colorbar()
+markers = ['o', 's']
+colors = ['r', 'g']
+for i, x in enumerate(X):
+    w = som.winner(x)
+    plot(w[0] + 0.5,
+         w[1] + 0.5,
+         markers[y[i]],
+         markeredgecolor = colors[y[i]],
+         markerfacecolor = 'None',
+         markersize = 10,
+         markeredgewidth = 2)
+show()
 
 # Finding the frauds
 ''' 
