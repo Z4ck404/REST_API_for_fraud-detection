f8761d5e05d1c44ef5b52c73f68fb1b2f9ce6ab1
Zakaria EL BAZI <za.elbazi@insea.ac.ma>
2019-04-03 11:07:09 +0200

    Update som_fraud.py

--- som_fraud.py
+++ som_fraud.py
@@ -1,1 +1,32 @@
+# Self Organizing Map
+
+# Importing the libraries
+import numpy as np
+import pandas as pd
+
+# Importing the dataset
+dataset = pd.read_csv('creditcard.csv')
+X = dataset.iloc[:, :-1].values
+y = dataset.iloc[:, -1].values
+
+# Feature Scaling
+from sklearn.preprocessing import MinMaxScaler
+sc = MinMaxScaler(feature_range = (0, 1))
+X = sc.fit_transform(X)
+
+# Training the SOM
+from minisom import MiniSom
+som = MiniSom(x = 10, y = 10, input_len = 30, sigma = 1.0, learning_rate = 0.5)
+som.random_weights_init(X)
+som.train_random(data = X, num_iteration = 100)
+
+
+# Finding the frauds
+''' 
+After visualizing the map , we noticed that the fraudulant transactions are behind t
+neuron (9,1) . 
+'''
+mappings = som.win_map(X)
+frauds = mappings[(9,1)]
+frauds = sc.inverse_transform(frauds)
 
